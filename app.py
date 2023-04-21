c8818af30b7732d9a456e4ce65bedacda285e165
Victoria Stavish <Vstavish@terpmail.umd.edu>
2023-04-21 19:15:46 +0000

    add owner column

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import requests
 import json
 import sqlite3
 
-# establish a connection to the database
+# Connect to a sql database called inspections.db
 conn = sqlite3.connect('inspections.db')
 cursor = conn.cursor()
 
-# create a table to hold the data
+# create a table that will hold the data scraped from the website
 create_table_sql = """
 CREATE TABLE inspections (
     establishment_id VARCHAR(255),
@@ -15,27 +15,29 @@ CREATE TABLE inspections (
     inspection_date DATE,
     inspection_results VARCHAR(255),
     zip VARCHAR(255),
-    address_line_1 VARCHAR(255)
+    address_line_1 VARCHAR(255),
+    owner VARCHAR(255)
 )
 """
 cursor.execute(create_table_sql)
 
+# pull the data from the website. I'm using ?$order=inspection_date DESC to make the output chronological and I;m using $limit=2000 for no particular reason but that is what signifies how many rows to pull from the API
 url = "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json?$order=inspection_date DESC&$limit=2000"
 
 response = requests.get(url)
 
 if response.status_code == 200:
     data = json.loads(response.text)
-
+# Here I'm giving a heads up which columns I'd like to grab. Right now I'm just grabbing basic information, but once I understand the database more I plan to also pull the columns that detail exactly which type of violation a restauarant did/didn't have. That will be an additional ~16 rows 
     for row in data:
-        print(row["establishment_id"], row["name"], row["category"], row["inspection_date"], row["inspection_results"], row["zip"], row["address_line_1"])
+        print(row["establishment_id"], row["name"], row["category"], row["inspection_date"], row["inspection_results"], row["zip"], row["address_line_1"], row["owner"])
 
-        # insert each row of data into the database
+        # insert each row of pulled data into the database
         insert_sql = """
         INSERT INTO inspections
-        (establishment_id, name, category, inspection_date, inspection_results, zip, address_line_1)
+        (establishment_id, name, category, inspection_date, inspection_results, zip, address_line_1, owner)
         VALUES
-        (?, ?, ?, ?, ?, ?, ?)
+        (?, ?, ?, ?, ?, ?, ?, ?)
         """
         values = (
             row["establishment_id"],
@@ -44,15 +46,16 @@ if response.status_code == 200:
             row["inspection_date"],
             row["inspection_results"],
             row["zip"],
-            row["address_line_1"]
+            row["address_line_1"],
+            row["owner"]
         )
         cursor.execute(insert_sql, values)
     
-    # commit changes to the database
+    # commit these changes to the database
     conn.commit()
     print(f"Inserted {len(data)} rows into the database.")
 else:
     print("Failed to retrieve data")
 
-# close the database connection
+# close the connection to the database
 conn.close()
