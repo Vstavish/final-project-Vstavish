7383869bb89c9da498b3156b8150735698490ed7
Victoria Stavish <Vstavish@terpmail.umd.edu>
2023-04-21 18:53:52 +0000

    scraped table, now putting in database

--- app.py
+++ app.py
@@ -1,31 +1,31 @@
 import requests
-from bs4 import BeautifulSoup
-import time
-import urllib.request
-from pprint import pprint
-import csv
-import os
 import json
+import sqlite3
 
-url = "https://data.princegeorgescountymd.gov/Health/Food-Inspection/umjn-t2iz"
+# establish a connection to the database
+conn = sqlite3.connect('inspections.db')
+cursor = conn.cursor()
+
+# create a table to hold the data
+create_table_sql = """
+CREATE TABLE inspections (
+    establishment_id VARCHAR(255),
+    name VARCHAR(255),
+    category VARCHAR(255),
+    inspection_date DATE,
+    inspection_results VARCHAR(255),
+    zip VARCHAR(255),
+    address_line_1 VARCHAR(255)
+)
+"""
+
+url = "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json?$order=inspection_date DESC&$limit=2000"
+
 response = requests.get(url)
 
-# Parse the HTML content using Beautiful Soup
-soup = BeautifulSoup(response.content, "html.parser")
-
-# Find the table element
-table = soup.find("table")
-
-# Check if the table was found
-if table is None:
-    print("Could not find table")
+if response.status_code == 200:
+    data = json.loads(response.text)
+    for row in data:
+        print(row["establishment_id"], row["name"], row["category"], row["inspection_date"], row["inspection_results"], row["zip"], row["address_line_1"])
 else:
-    # Find all the rows in the table
-    rows = table.find_all("tr")
-
-    # Loop over the rows and print out the data in each cell
-    for row in rows:
-        cells = row.find_all("td")
-        for cell in cells:
-            print(cell.text.strip(), end="\t")
-        print()
+    print("Failed to retrieve data")
