11f71a598ed048e49f040876684c8efdbf878562
Victoria Stavish <Vstavish@terpmail.umd.edu>
2023-05-01 16:37:22 +0000

    formatting browser

--- app.py
+++ app.py
@@ -1,61 +1,42 @@
-import requests
-import json
+from flask import Flask, render_template
 import sqlite3
 
-# Connect to a sql database called inspections.db
-conn = sqlite3.connect('inspections.db')
-cursor = conn.cursor()
+app = Flask(__name__)
 
-# create a table that will hold the data scraped from the website
-create_table_sql = """
-CREATE TABLE inspections (
-    establishment_id VARCHAR(255),
-    name VARCHAR(255),
-    category VARCHAR(255),
-    inspection_date DATE,
-    inspection_results VARCHAR(255),
-    zip VARCHAR(255),
-    address_line_1 VARCHAR(255),
-    owner VARCHAR(255)
-)
-"""
-cursor.execute(create_table_sql)
+@app.route("/")
+def index():
+    conn = sqlite3.connect('inspections.db')
+    # Define the SQL query to retrieve the establishment IDs and their counts
+    query = """
+    SELECT establishment_id, COUNT(*) as count, name, zip, owner
+    FROM inspections
+    GROUP BY establishment_id
+    ORDER BY count DESC
+    LIMIT 10;
+    """
+    # Execute the SQL query and fetch the results
+    cursor = conn.execute(query)
+    results = cursor.fetchall()
 
-# pull the data from the website. I'm using ?$order=inspection_date DESC to make the output chronological and I;m using $limit=2000 for no particular reason but that is what signifies how many rows to pull from the API
-url = "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json?$order=inspection_date DESC&$limit=2000"
+    # Execute the SQL query and fetch the results
+    cursor = conn.execute(query)
+    results = cursor.fetchall()
 
-response = requests.get(url)
+    # Close the connection to the database
+    conn.close()
 
-if response.status_code == 200:
-    data = json.loads(response.text)
-# Here I'm giving a heads up which columns I'd like to grab. Right now I'm just grabbing basic information, but once I understand the database more I plan to also pull the columns that detail exactly which type of violation a restauarant did/didn't have. That will be an additional ~16 rows 
-    for row in data:
-        print(row["establishment_id"], row["name"], row["category"], row["inspection_date"], row["inspection_results"], row["zip"], row["address_line_1"], row["owner"])
+    # Render the template with the data
+    return render_template('index.html', rows=results)
 
-        # insert each row of pulled data into the database
-        insert_sql = """
-        INSERT INTO inspections
-        (establishment_id, name, category, inspection_date, inspection_results, zip, address_line_1, owner)
-        VALUES
-        (?, ?, ?, ?, ?, ?, ?, ?)
-        """
-        values = (
-            row["establishment_id"],
-            row["name"],
-            row["category"],
-            row["inspection_date"],
-            row["inspection_results"],
-            row["zip"],
-            row["address_line_1"],
-            row["owner"]
-        )
-        cursor.execute(insert_sql, values)
-    
-    # commit these changes to the database
-    conn.commit()
-    print(f"Inserted {len(data)} rows into the database.")
-else:
-    print("Failed to retrieve data")
 
-# close the connection to the database
-conn.close()
+    # Format the results as a response
+    response = ''
+    for result in results:
+        response += f'{result[0]}: {result[1]}\n'
+    return response
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
---
+++ database.py
@@ -0,0 +1,61 @@
+import requests
+import json
+import sqlite3
+
+# Connect to a sql database called inspections.db
+conn = sqlite3.connect('inspections.db')
+cursor = conn.cursor()
+
+# create a table that will hold the data scraped from the website
+create_table_sql = """
+CREATE TABLE inspections (
+    establishment_id VARCHAR(255),
+    name VARCHAR(255),
+    category VARCHAR(255),
+    inspection_date DATE,
+    inspection_results VARCHAR(255),
+    zip VARCHAR(255),
+    address_line_1 VARCHAR(255),
+    owner VARCHAR(255)
+)
+"""
+#cursor.execute(create_table_sql)
+
+# pull the data from the website. I'm using ?$order=inspection_date DESC to make the output chronological and I;m using $limit=2000 for no particular reason but that is what signifies how many rows to pull from the API
+url = "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json?$order=inspection_date DESC&$limit=2000"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = json.loads(response.text)
+# Here I'm giving a heads up which columns I'd like to grab. Right now I'm just grabbing basic information, but once I understand the database more I plan to also pull the columns that detail exactly which type of violation a restauarant did/didn't have. That will be an additional ~16 rows unfortunately lol
+    for row in data:
+        print(row["establishment_id"], row["name"], row["category"], row["inspection_date"], row["inspection_results"], row["zip"], row["address_line_1"], row["owner"])
+
+        # insert each row of pulled data into the database
+        insert_sql = """
+        INSERT INTO inspections
+        (establishment_id, name, category, inspection_date, inspection_results, zip, address_line_1, owner)
+        VALUES
+        (?, ?, ?, ?, ?, ?, ?, ?)
+        """
+        values = (
+    row["establishment_id"],
+    row["name"].lower(),
+    row["category"].lower(),
+    row["inspection_date"],
+    row["inspection_results"].lower(),
+    row["zip"],
+    row["address_line_1"].lower(),
+    row["owner"].lower()
+)
+        cursor.execute(insert_sql, values)
+    
+    # commit these changes to the database
+    conn.commit()
+    print(f"Inserted {len(data)} rows into the database.")
+else:
+    print("Failed to retrieve data")
+
+# close the connection to the database
+conn.close()
