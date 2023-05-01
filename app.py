831fe0ab82e6ace2ab5c55659d19216ce38484fa
Victoria Stavish <Vstavish@terpmail.umd.edu>
2023-05-01 17:31:40 +0000

    search bar

--- app.py
+++ app.py
@@ -1,42 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import sqlite3
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    conn = sqlite3.connect('inspections.db')
-    # Define the SQL query to retrieve the establishment IDs and their counts
-    query = """
-    SELECT establishment_id, COUNT(*) as count, name, zip, owner
-    FROM inspections
-    GROUP BY establishment_id
-    ORDER BY count DESC
-    LIMIT 10;
-    """
-    # Execute the SQL query and fetch the results
-    cursor = conn.execute(query)
-    results = cursor.fetchall()
-
-    # Execute the SQL query and fetch the results
-    cursor = conn.execute(query)
-    results = cursor.fetchall()
-
-    # Close the connection to the database
-    conn.close()
-
-    # Render the template with the data
-    return render_template('index.html', rows=results)
-
-
-    # Format the results as a response
-    response = ''
-    for result in results:
-        response += f'{result[0]}: {result[1]}\n'
-    return response
-
+    if request.method == "POST":
+        # Handle the search query
+        search_query = request.form["search_query"]
+        conn = sqlite3.connect('inspections.db')
+        query = """
+        SELECT establishment_id, name, COUNT(*) AS count, zip, owner
+        FROM inspections
+        WHERE name LIKE ?
+        OR owner LIKE ?
+        GROUP BY establishment_id
+        ORDER BY count DESC
+        """
+        cursor = conn.execute(query, ('%'+search_query+'%', '%'+search_query+'%'))
+        results = cursor.fetchall()
+        conn.close()
+        return render_template('index.html', rows=results, search_query=search_query)
+    else:
+        # Show the default index page
+        conn = sqlite3.connect('inspections.db')
+        query = """
+        SELECT establishment_id, name, COUNT(*) AS count, zip, owner
+        FROM inspections
+        WHERE inspection_results LIKE '%critical violations observed%'
+        GROUP BY establishment_id
+        ORDER BY count DESC
+        LIMIT 10;
+        """
+        cursor = conn.execute(query)
+        results = cursor.fetchall()
+        conn.close()
+        return render_template('index.html', rows=results)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=8000)
+
 
 
