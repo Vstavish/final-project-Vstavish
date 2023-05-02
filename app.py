0468eab64c00ce9ad402b299499cef23ca0f79bf
Victoria Stavish <Vstavish@terpmail.umd.edu>
2023-05-02 17:38:19 +0000

    fix numbers search

--- app.py
+++ app.py
@@ -9,15 +9,36 @@ def index():
         # Handle the search query
         search_query = request.form["search_query"]
         conn = sqlite3.connect('inspections.db')
-        query = """
-        SELECT establishment_id, name, COUNT(*) AS count, zip, owner
-        FROM inspections
-        WHERE name LIKE ?
-        OR owner LIKE ?
-        GROUP BY establishment_id
-        ORDER BY count DESC
-        """
-        cursor = conn.execute(query, ('%'+search_query+'%', '%'+search_query+'%'))
+        if len(search_query) == 5 and search_query.isdigit():
+            # Search by zip code
+            query = """
+            SELECT establishment_id, name, COUNT(*) AS count, zip, owner
+            FROM inspections
+            WHERE zip = ?
+            GROUP BY establishment_id
+            ORDER BY count DESC
+            """
+            cursor = conn.execute(query, (search_query,))
+        elif len(search_query) == 4 and search_query.isdigit():
+            # Search by establishment ID
+            query = """
+            SELECT establishment_id, name, COUNT(*) AS count, zip, owner
+            FROM inspections
+            WHERE establishment_id = ?
+            GROUP BY establishment_id
+            """
+            cursor = conn.execute(query, (search_query,))
+        else:
+            # Search by name or owner
+            query = """
+            SELECT establishment_id, name, COUNT(*) AS count, zip, owner
+            FROM inspections
+            WHERE name LIKE ?
+            OR owner LIKE ?
+            GROUP BY establishment_id
+            ORDER BY count DESC
+            """
+            cursor = conn.execute(query, ('%'+search_query+'%', '%'+search_query+'%'))
         results = cursor.fetchall()
         conn.close()
         return render_template('index.html', rows=results, search_query=search_query)
@@ -36,6 +57,7 @@ def index():
         results = cursor.fetchall()
         conn.close()
         return render_template('index.html', rows=results)
+
 
 if __name__ == "__main__":
     app.run(port=8000)
